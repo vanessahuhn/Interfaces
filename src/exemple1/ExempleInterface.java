7d6a2a64840cc43f087da88825d9fa06076318b0
vanessahuhn <vanessa.huhn@gmail.com>
2018-08-06 15:45:06 +0200

    correction exemple 1

--- src/exemple1/ExempleInterface.java
+++ src/exemple1/ExempleInterface.java
@@ -2,7 +2,14 @@ package exemple1;
 
 public interface ExempleInterface {
 	
+	//signature des méthodes
 	public int minimum();
 	public int maximum();
+	
+	//signatures des getters et setters
+	public int getV1();
+	public void setV1(int value);
+	public int getV2();
+	public void setV2(int value);
 
 }
